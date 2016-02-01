a5e4a36c1a827a2b39700b53ab3961494bd78cd6
bizqu002 <bizqu002@fiu.edu>
2016-02-01 04:54:30 -0500

    modified:   first-sprint.html 	modified:   first-sprint.js

--- first-sprint.js
+++ first-sprint.js
@@ -42,31 +42,35 @@ if (Meteor.isClient) {
 			zip: zipVar,
 			telephone: telephoneVar 
         },
+				
         function(err)//in case we have error, such as existing user
-        {
-          if(err)//present this message 
-            Session.set("enemyLogOut", "Error, email is allready taken");
-          else
-           { //otherwise, change the message, we succed to store the user
-            Session.set("enemyLogOut", "");
-            Session.set("enemyLogIn", "");
-          }
-        });
-    }
+		{
+          	if(err)//present this message 
+            	Session.set("enemyLogOut", "Error: email is already taken");
+          	else
+           	{ 
+			    //otherwise, change the message, we succed to store the user
+            	Session.set("enemyLogOut", "");
+            	Session.set("enemyLogIn", "");
+			}
+       	});
+	}
 });
-Template.register.helpers({//to present the message in the registration page
-  RegistrationError: function()
-  {
-    return Session.get("enemyLogOut");
-  }
-})
 	
-	Template.login.events({//
+		
+	Template.register.helpers({//to present the message in the registration page
+		RegistrationError: function()
+		{
+			return Session.get("enemyLogOut");
+		}
+	});
+	
+	Template.login.events({// 
     'submit form': function(event){//when someone submit hsi login information
         event.preventDefault();
         var emailVar = event.target.loginEmail.value;
         var passwordVar = event.target.loginPassword.value;        
-        Meteor.loginWithPassword(emailVar, passwordVar, function(err)
+		Meteor.loginWithPassword(emailVar, passwordVar, function(err)
         {
           if(err) //check if we have an error                   
           {
@@ -81,91 +85,90 @@ Template.register.helpers({//to present the message in the registration page
          
         });//log in with the password
     }
-  });
+  	});
 
-  Template.login.helpers({
-     LogError: function()//show the message for the login error
-    {
-       return Session.get("enemyLogIn");
-     }
-  });
+  	Template.login.helpers({
+     	LogError: function()//show the message for the login error
+    	{
+       		return Session.get("enemyLogIn");
+     	}
+  	});
 
 	
 	Template.dashboard.events({//for the log out page
-    'click .logout': function(event){
-        event.preventDefault();
-        Meteor.logout();
-    }
-});
+		'click .logout': function(event){
+			event.preventDefault();
+			Meteor.logout();
+		}
+	});
 	
 	   	
 	
 	/* client/accounts/recover-password.js */
 
-// Ensure we have the token to pass into the template when it's present
-if (Accounts._resetPasswordToken) {  
-  Session.set('resetPasswordToken', Accounts._resetPasswordToken);
-}
+	// Ensure we have the token to pass into the template when it's present
+	if (Accounts._resetPasswordToken) {  
+	  Session.set('resetPasswordToken', Accounts._resetPasswordToken);
+	}
 
-Template.RecoverPassword.helpers({  
-  resetPassword: function() {
-    return Session.get('resetPasswordToken');
-  }
-});
+	Template.RecoverPassword.helpers({  
+	  resetPassword: function() {
+		return Session.get('resetPasswordToken');
+	  }
+	});
 
-Template.RecoverPassword.events({  
-  'submit #forgot-password': function(event, template) {
-    var email = template.find('#user-email'),
-      message;
+	Template.RecoverPassword.events({  
+	  'submit #forgot-password': function(event, template) {
+		var email = template.find('#user-email'),
+		  message;
 
-    // You will probably want more robust validation than this!
-    if (email) {
-      // This will send a link to the address which, upon clicking, prompts the
-      //user to enter a new password.
-      Accounts.forgotPassword(email);
-      message = 'Sent a reset password link to ' + email + '.';
-    } else {
-      message = 'Please enter a valid email address.'
-    }
+		// You will probably want more robust validation than this!
+		if (email) {
+		  // This will send a link to the address which, upon clicking, prompts the
+		  //user to enter a new password.
+		  Accounts.forgotPassword(email);
+		  message = 'Sent a reset password link to ' + email + '.';
+		} else {
+		  message = 'Please enter a valid email address.'
+		}
 
-    // Inform the user.
-    template.find('#form-messages').html(message);
+		// Inform the user.
+		template.find('#form-messages').html(message);
 
-    return false;
-  },
-  'submit #set-new-password': function (event, template) {
-    // Proper decoupled validation would be much nicer than this
-    var password = template.find('#new-password').value,
-      passwordTest = new RegExp("(?=.{6,}).*", "g");
+		return false;
+	  },
+	  'submit #set-new-password': function (event, template) {
+		// Proper decoupled validation would be much nicer than this
+		var password = template.find('#new-password').value,
+		  passwordTest = new RegExp("(?=.{6,}).*", "g");
 
-    // If the password is valid, we can reset it.
-    if (passwordTest.test(password)) {
-      Accounts.resetPassword(
-        Session.get('resetPasswordToken'),
-        password,
-        function (error) {
-          if (err) {
-            template.find('#form-messages').html('There was a problem resetting your password.');
-          } else {
-            // Get rid of the token so the forms render properly when they come back.
-            Session.set('resetPasswordToken', null);
-          }
-        })
-      } else {
-      // Looks like they blew it
-      template.find('#form-messages').html('Your password is too weak!');
-    }
+		// If the password is valid, we can reset it.
+		if (passwordTest.test(password)) {
+		  Accounts.resetPassword(
+			Session.get('resetPasswordToken'),
+			password,
+			function (error) {
+			  if (err) {
+				template.find('#form-messages').html('There was a problem resetting your password.');
+			  } else {
+				// Get rid of the token so the forms render properly when they come back.
+				Session.set('resetPasswordToken', null);
+			  }
+			})
+		  } else {
+		  // Looks like they blew it
+		  template.find('#form-messages').html('Your password is too weak!');
+		}
 
-    return false;
-  }
-});
+		return false;
+	  }
+	});
 	
 }
 
 
 if (Meteor.isServer) {
     // code here will only be run on the server
-
-    
+	
 }
 
