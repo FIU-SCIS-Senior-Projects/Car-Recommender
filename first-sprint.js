c343ba0fcf9f1f27f5f10ebfa96d048aa2a22c96
bizqu002 <bizqu002@fiu.edu>
2016-02-08 05:51:22 -0500

    modified:   first-sprint.html 	modified:   first-sprint.js

--- first-sprint.js
+++ first-sprint.js
@@ -39,7 +39,47 @@ if (Meteor.isClient) {
   //message for login and registration
 	Session.set("enemyLogIn", "");//login
 	Session.set("enemyLogOut", "");//registration
+	
+	
+	Template.sell.helpers({  
+    data: { 
+		
+	year: [{y: "2016"}, {y: "2015"}, {y: "2014"}, {y: "2013"}, {y: "2012"}, {y: "2011"}, {y: "2010"}, 
+  		   {y: "2009"}, {y: "2008"}, {y: "2007"}, {y: "2006"}, {y: "2005"}, {y: "2004"}, {y: "2003"}, {y: "2002"}, {y: "2001"},  {y: "2000"},
+		   {y: "1999"}, {y: "1998"}, {y: "1997"}, {y: "1996"}, {y: "1995"}, {y: "1994"}, {y: "1993"}, {y: "1992"}, {y: "1991"},  {y: "1990"}],
+		
+	make: [	{car: "Acura"}, {car: "Alfa Romeo"}, {car: "Aston Martin"}, {car: "Audi"}, {car: "Bentley"}, {car:"BMW"}, {car: "Buick"}, 				   	{car:"Cadillac"}, {car:"Chevrolet"}, {car:"Chrysler"}, {car:"Dodge"}, {car:"Ferrari"}, {car:"Ford"}, {car:"GMC"},
+		  	{car:"Honda"}, {car:"HUMMER"}, {car:"Hyundai"}, {car:"Infiniti"}, {car:"Isuzu"}, {car:"Jaguar"}, {car:"Jeep"},
+		  	{car:"Kia"}, {car:"Lamborghini"}, {car:"Land Rover"}, {car:"Lexus"}, {car:"Lincoln"}, {car:"Lotus"}, {car:"Maserati"},
+		  	{car:"Maybach"}, {car:"Mazda"}, {car: "Mercedes-Benz"}, {car:"Mercury"}, {car:"MINI"}, {car: "Mitsubishi"},  {car:"Nissan"},	
+		  	{car:"Oldsmobile"}, {car:"Panoz"}, {car:"Pontiac"}, {car:"Porsche"}, {car:"Rolls-Royce"}, {car:"Saab"}, {car:"Saturn"},
+		  	{car:"Scion"}, {car:"Subaru"}, {car:"Suzuki"}, {car:"Toyota"}, {car:"Volkswagen"}, {car:"Volvo"} ],
+		
+	style: [{s:"Convertible"}, {s:"Coupe"}, {s: "Crossover"}, {s: "Diesel"}, {s:"Hatchback"}, {s: "Hybrid/Electric"}, {s: "Luxury"}, 
+			{s: "Minivan/Van"}, {s: "Sedan"}, {s: "SUV"}, {s: "Truck"}, {s: "Wagon"}],
+		
+	mileage: [{m: "<10"}, {m: "10"}, {m: "11"}, {m: "12"}, {m: "13"}, {m: "14"}, {m: "15"}, {m: "16"}, {m: "17"}, {m: "18"}, {m: "19"},
+			  {m: "20"}, {m: "21"}, {m: "22"}, {m: "23"}, {m: "24"}, {m: "25"}, {m: "26"}, {m: "27"}, {m: "28"}, {m: "29"},
+			  {m: "30"}, {m: "31"}, {m: "32"}, {m: "33"}, {m: "34"}, {m: "35"}, {m: "36"}, {m: "37"}, {m: "38"}, {m: "39"},
+			  {m: "40"}, {m: "41"}, {m: "42"}, {m: "43"}, {m: "44"}, {m: "45"}, {m: "46"}, {m: "47"}, {m: "48"}, {m: "49"},
+			  {m: "50"}, {m: "51"}, {m: "52"}, {m: "53"}, {m: "54"}, {m: "55"}, {m: "56"}, {m: "57"}, {m: "58"}, {m: "59"},
+			  {m: "60"}, {m: "61"}, {m: "62"}, {m: "63"}, {m: "64"}, {m: "65"}, {m: "66"}, {m: "67"}, {m: "68"}, {m: "69"},
+			  {m: "70"}, {m: "71"}, {m: "72"}, {m: "73"}, {m: "74"}, {m: "75"}, {m: ">75"}
+			 ],
+		
+	cylinder: [	{c: "2-Cylinder"}, {c: "4-Cylinder"}, {c: "6-Cylinder"}, {c: "8-Cylinder"}, {c: "10-Cylinder"}, {c: "12-Cylinder"}],
+	
+	transmission: [	{t: "Automatic"}, {t: "Manual"}],
+		
+	ecolor: [{c: "Black"}, {c: "Blue"}, {c: "Brown"}, {c: "Gold"}, {c: "Gray"}, {c: "Green"}, {c:"Orange"}, {c:"Purple"}, {c:"Red"}, 		
+			{c:"Silver"}, {c:"Tan"}, {c:"White"}, {c:"Yellow"} ],		
+	
+	icolor: [{c: "Black"}, {c: "Gray"}, {c:"Tan"}]
+		
+	}
+	});
 
+	
     Template.register.events({
     'submit form': function(event) {//insert the user name to database
         event.preventDefault();
